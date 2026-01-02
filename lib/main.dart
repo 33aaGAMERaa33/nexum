af5ded231a99d0625bd25e894e258e177373b636
33aaGAMERaa33 <137464815+33aaGAMERaa33@users.noreply.github.com>
2026-01-02 18:28:49 -0300

    a

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:nexum_core/run_app.dart';
+import 'package:nexum_core/widgets/align.dart';
+import 'package:nexum_core/widgets/debug_box.dart';
 import 'package:nexum_core/widgets/flex.dart';
 import 'package:nexum_core/widgets/framework.dart';
 import 'package:nexum_core/widgets/rich_text.dart';
+import 'package:nexum_core/widgets/single_child_scroll_view.dart';
+import 'package:nexum_core/widgets/sized_box.dart';
 
 void main() {
   runApp(const Example());
@@ -12,10 +16,24 @@ class Example extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      childrens: [
-        const RichText("Olá, Mundo!"),
-      ]
+    final List<Widget> childrens = [];
+
+    for(int i = 1; i <= 100; i++) {
+      childrens.add(DebugBox(
+        child: SizedBox(
+          width: 100,
+          height: 100,
+          child: Center(
+            child: RichText("Olá, Mundo!")
+          )
+        ),
+      ));
+    }
+
+    return Center(
+        child: SingleChildScrollView(
+          child: Column(childrens: childrens)
+        )
     );
   }
 }
